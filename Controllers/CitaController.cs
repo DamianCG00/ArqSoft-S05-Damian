b0cf5e87fb0e5ce9adb4e22946009742a3222d66
DamianCG00 <marco.coba110107@gmail.com>
2026-06-02 08:22:17 -0600

    Agrego de botones

---
+++ Controllers/CitaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Citas_app.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Citas_app.Controllers
+{
+    public class CitaController : Controller
+    {
+        
+        private static List<Cita> citas = new List<Cita>
+        {
+            
+            new Cita { Id = 1, PacienteId = "1", MedicoId = 1, Fecha = 20260615, Hora = 1030, Motivo = 1, Estado = 1 },
+            new Cita { Id = 2, PacienteId = "1", MedicoId = 2, Fecha = 20260620, Hora = 1200, Motivo = 2, Estado = 1 },
+            new Cita { Id = 3, PacienteId = "2", MedicoId = 1, Fecha = 20260618, Hora = 1645, Motivo = 3, Estado = 2 }
+        };
+
+        public IActionResult Index()
+        {
+            return View(citas);
+        }
+
+        
+        public IActionResult PorPaciente(string pacienteId)
+        {
+            var citasDelPaciente = citas.Where(c => c.PacienteId == pacienteId).ToList();
+
+            return View(citasDelPaciente);
+        }
+    }
+}
---
+++ Models/Cita.cs
@@ -0,0 +1,23 @@
+﻿namespace Citas_app.Models
+{
+    public class Cita
+    {
+
+        public int Id { get; set; }
+
+        public string? PacienteId { get; set; }
+
+        public int MedicoId { get; set; }
+
+        public int Fecha { get; set; }
+
+        public int Hora { get; set; }
+
+        public int Motivo { get; set; }
+
+        public int Estado { get; set; } 
+
+
+
+    }
+}
